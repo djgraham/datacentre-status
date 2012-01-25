fd8ed37faded60aa229d5a28e640da25df1bd98a
David Graham <github@djgraham.co.uk>
2012-01-25 23:44:29 +0000

    Initial base rails 3.2.install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MelbourneStatus::Application.config.secret_token = '93929d1f65dd5e3ea2bcfcd0a0eba2b7a329a0ca45dffa224562f25530ce1d5e3b57dec6d603ef96a0654b7849ed227cb931f849267ce11ab8fd00513ca6901e'

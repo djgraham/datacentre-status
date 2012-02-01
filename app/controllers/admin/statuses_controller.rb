21aba001af14c3a7dbd5af66022fd61c1667a5ba
David Graham <github@djgraham.co.uk>
2012-02-01 03:28:34 +0000

    Fix stories and forms for statuses

--- app/controllers/admin/statuses_controller.rb
+++ app/controllers/admin/statuses_controller.rb
@@ -1,5 +1,6 @@
 class Admin::StatusesController < ApplicationController
 
+  before_filter :load_status, :except => [:index, :new, :create]
   respond_to :html
 
   def index
@@ -7,7 +8,34 @@ class Admin::StatusesController < ApplicationController
   end
 
   def edit
-    @status = Status.find(params[:id])
+
+  end
+
+  def new
+    @status = Status.new
+  end
+
+  def update
+    if @status.update_attributes(params[:status])
+      redirect_to admin_statuses_path, :flash => { :info => "Status saved" }
+    else
+      render :action => :edit
+    end
+  end
+
+  def create
+    @status = Status.new
+    if @status.update_attributes(params[:status])
+      redirect_to admin_statuses_path, :flash => { :info => "Status created" }
+    else
+      render :action => :edit
+    end
+  end
+
+  protected
+
+  def load_status
+    @status = Status.find params[:id] 
   end
 
 end
--- features/step_definitions/status_steps.rb
+++ features/step_definitions/status_steps.rb
@@ -18,21 +18,24 @@ When /^I click "([^"]*)"$/ do |arg1|
 end
 
 When /^I assign resolved flag$/ do
-  pending # express the regexp above with the code you wish you had
-  #click_button "
+  #pending # express the regexp above with the code you wish you had
+  check "status_resolved"
+  click_button "Save"
 end
 
 Then /^"([^"]*)" should have a resolved flag$/ do |arg1|
-  @status = Status.where(:name => arg1)
+  #@status = Status.where(:name => arg1)
   @status.should be_resolved
 end
 
 When /^I revoke resolved flag$/ do
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
+  uncheck "status_resolved"
+  click_button "Save"
 end
 
 Then /^"([^"]*)" should not have a resolved flag$/ do |arg1|
-  @status = Status.where(:name => arg1)
+  #@status = Status.where(:name => arg1)
   @status.should_not be_resolved
 end
 

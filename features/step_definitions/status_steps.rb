64bbbef2e6a0375246e827ffb0b539889a4c82f6
David Graham <github@djgraham.co.uk>
2012-02-01 03:30:59 +0000

    clean up steps

--- features/step_definitions/status_steps.rb
+++ features/step_definitions/status_steps.rb
@@ -1,6 +1,6 @@
 Given /^I am logged in as an administrator$/ do
   #@admin = a_saved User, :email => 'hello@example.com', :admin => true
-  #visit "/admin/statuses"
+  # we don't have any administration users yet..
 end
 
 Given /^there are statuses within the system$/ do
@@ -12,30 +12,24 @@ When /^I visit the administrator statuses page$/ do
 end
 
 When /^I click "([^"]*)"$/ do |arg1|
-  #pending # express the regexp above with the code you wish you had
-  #@status = Status.where(:name => arg1).first
   visit "/admin/statuses/#{@status.id}/edit"
 end
 
 When /^I assign resolved flag$/ do
-  #pending # express the regexp above with the code you wish you had
   check "status_resolved"
   click_button "Save"
 end
 
 Then /^"([^"]*)" should have a resolved flag$/ do |arg1|
-  #@status = Status.where(:name => arg1)
   @status.should be_resolved
 end
 
 When /^I revoke resolved flag$/ do
-  #pending # express the regexp above with the code you wish you had
   uncheck "status_resolved"
   click_button "Save"
 end
 
 Then /^"([^"]*)" should not have a resolved flag$/ do |arg1|
-  #@status = Status.where(:name => arg1)
   @status.should_not be_resolved
 end
 
